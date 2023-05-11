e79b6f2537322940bab1a0d97ddac3f7db431090
shama-10 <shammaas.cs@gmail.com>
2023-05-12 00:56:55 +0500

    still
    working ......

--- code/backend/database/index.js
+++ code/backend/database/index.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectionString ="mongodb+srv://bugAuthor:coinbounce@cluster0.m9tktcr.mongodb.net/?retryWrites=true&w=majority";
+const connectionString ="mongodb+srv://bugAuthor:ZBbDF6P32XYg9wz4@cluster0.m9tktcr.mongodb.net/coin-bounce?retryWrites=true&w=majority";
 
 const dbConnect = async () => {
-    try {
-        const conn = await mongoose.connect(connectionString, {
-            connectTimeoutMS: 30000 // 30 seconds
-        });
-        console.log(`Database Connected to host: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error}`);
-    }
-};
+   try {
+    const conn  = await mongoose.connect(connectionString);
+    console.log(`Database Connected to host:'${conn,connection.host}`);
+
+   } catch (error) {
+    console.log(`Error:${error}`);
+   }
+}
 
 module.exports = dbConnect;
 
