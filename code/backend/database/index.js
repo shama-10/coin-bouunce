ac632db8e5d258e2f3519d152c9ec932b1b62a07
shama-10 <shammaas.cs@gmail.com>
2023-05-12 12:31:51 +0500

    optimize code
    By creating config file for port  and DB connection

--- code/backend/database/index.js
+++ code/backend/database/index.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const {MONGODB_CONNECTION_STRING} = require('../config/index');
 
-const connectionString ="mongodb+srv://bugAuthor:ZBbDF6P32XYg9wz4@cluster0.m9tktcr.mongodb.net/coin-bounce?retryWrites=true&w=majority";
+//const connectionString ="mongodb+srv://bugAuthor:ZBbDF6P32XYg9wz4@cluster0.m9tktcr.mongodb.net/coin-bounce?retryWrites=true&w=majority";
 
 const dbConnect = async () => {
    try {
-    const conn  = await mongoose.connect(connectionString);
+   mongoose.set('strictQuery',false);
+    const conn  = await mongoose.connect(MONGODB_CONNECTION_STRING);
     console.log(`Database Connected to host:'${conn.connection.host}`);
 
    } catch (error) {
--- code/backend/server.js
+++ code/backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dbConnect = require('./database/index');
+const {PORT} = require('./config/index');
 
 const app = express();
 
-const PORT = 5000;
 
 dbConnect();
 
-app.get('/',(req,res) => res.json({msg:'Hello World 1234'}));
+app.get('/',(req,res) => res.json({msg:'Hello World;'}));
 
 app.listen(PORT, console.log(`Backend is runnung on PORT: ${PORT} `));
