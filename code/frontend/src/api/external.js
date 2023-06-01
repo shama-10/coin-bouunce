c0e193e78c51783d60bd531afc175bf8851e0bb5
shama-10 <133121758+shama-10@users.noreply.github.com>
2023-06-01 19:26:43 +0500

    working
    working on .env file
    and externalapi file
    not complete yet
    time 08:10:54

---
+++ code/frontend/src/api/external.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const NEWS_API_ENDPOINT =
+  "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json";
+  
+export const getNews = async () => {
+  let response;
+
+  try {
+    response = await axios.get(NEWS_API_ENDPOINT);
+    response = response.data.articles.slice(0, 15);
+  } catch (error) {}
+
+  return response;
+};
